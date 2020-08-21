e7ef3be68d7e3e7b971f54c7e36a17054407ea21
Mogball <jeffniu22@gmail.com>
2020-08-21 13:30:14 +0000

    added program annotation

--- oec/stencil.py
+++ oec/stencil.py
@@ -462,25 +462,15 @@ def raisePass(m):
     applyPartialConversion(m, [], target)
 
 ################################################################################
-# Test Area
+# Public API
 ################################################################################
 
-def laplace(a:numpy.ndarray, b:numpy.ndarray):
-  stencil.cast(a, [-4, -4, -4], [68, 68, 68])
-  stencil.cast(b, [-4, -4, -4], [68, 68, 68])
-  atmp = stencil.load(a)
-
-  def applyFcn(c) -> float:
-    return -4 * c[0, 0, 0] + c[-1, 0, 0] + c[1, 0, 0] + c[0, 1, 0] + c[0, -1, 0]
-
-  btmp = stencil.apply(atmp, applyFcn)
-  stencil.store(b, btmp, [0, 0, 0], [64, 64, 64])
-  return
-
-node = ast.parse(inspect.getsource(laplace))
-visitor = StencilProgramVisitor()
-m = visitor.visit(node)
-varAllocPass(m)
-raisePass(m)
-print(m)
-verify(m)
+def program(func):
+    node = ast.parse(inspect.getsource(func))
+    m = StencilProgramVisitor().visit(node)
+    varAllocPass(m)
+    raisePass(m)
+    verify(m)
+    def wrapper():
+        return m
+    return wrapper
---
+++ oec/test.py
@@ -0,0 +1,16 @@
+import stencil
+
+@stencil.program
+def laplace(a, b):
+  stencil.cast(a, [-4, -4, -4], [68, 68, 68])
+  stencil.cast(b, [-4, -4, -4], [68, 68, 68])
+  atmp = stencil.load(a)
+
+  def applyFcn(c) -> float:
+    return -4 * c[0, 0, 0] + c[-1, 0, 0] + c[1, 0, 0] + c[0, 1, 0] + c[0, -1, 0]
+
+  btmp = stencil.apply(atmp, applyFcn)
+  stencil.store(b, btmp, [0, 0, 0], [64, 64, 64])
+  return
+
+print(laplace())
